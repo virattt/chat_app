e7f89c3e988e87ec6621c6f23dc51dd774017542
virattt <virat.dot@gmail.com>
2023-05-21 21:03:25 -0400

    Try to stream LLM output via AsyncCallbackHandler

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,7 +1,13 @@
 import json
 import os
+import sys
+from typing import Optional, Any, Dict, List
+from uuid import UUID
 
 import django
+from asgiref.sync import async_to_sync
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.schema import BaseMessage
 
 from chat.agents.agent_factory import AgentFactory
 from chat.messages.chat_message_repository import ChatMessageRepository
@@ -29,7 +35,12 @@ class ChatConsumer(AsyncWebsocketConsumer):
         chat_id = self.scope['url_route']['kwargs'].get('chat_id')
 
         # Create the agent when the websocket connection with the client is established
-        self.agent = await self.agent_factory.create_agent(tool_names=["llm-math"], chat_id=chat_id)
+        self.agent = await self.agent_factory.create_agent(
+            tool_names=["llm-math"],
+            chat_id=chat_id,
+            streaming=True,
+            callback_handlers=[MyCustomHandler(self)],
+        )
 
         await self.accept()
 
@@ -58,3 +69,35 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.chat_message_repository.save_message(message=response, sender=MessageSender.AI.value, chat_id=chat_id)
 
         return response
+
+    def my_callback(self, message):
+        print("Callback received:", message)
+
+
+class MyCustomHandler(AsyncCallbackHandler):
+
+    def __init__(self, consumer):
+        self.consumer = consumer
+
+    async def on_chat_model_start(
+        self,
+        serialized: Dict[str, Any],
+        messages: List[List[BaseMessage]],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any
+    ) -> Any:
+        pass
+
+    async def on_llm_new_token(
+        self,
+        token: str,
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        **kwargs: Any,
+    ) -> None:
+        sys.stdout.write(token)
+        sys.stdout.flush()
+        async_to_sync(self.consumer.send)(text_data=token)
